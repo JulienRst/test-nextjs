179cdcd578a5033dd49465949a4a80dc875dfe0e
Julien Rousset <julien.rousset@matters.tech>
2024-01-10 15:35:22 +0100

    chapter-fifteen

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { sql } from "@vercel/postgres";
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
@@ -86,7 +88,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to delete Invoice');
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (error) {
@@ -95,4 +96,22 @@ export async function deleteInvoice(id: string) {
     }
   }
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData)
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default: 
+          return 'Somehting went wrong.';
+      }
+    }
+  }
 }
